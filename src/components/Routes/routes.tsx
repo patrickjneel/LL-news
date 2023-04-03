45a49ec4395ffddad2ddf9f234cd8787f290425c
Pat Neel <patrick.neel@colorado.edu>
2023-04-03 17:33:08 -0600

    add in lazy loading for routes

--- src/components/Routes/routes.tsx
+++ src/components/Routes/routes.tsx
@@ -1,11 +1,12 @@
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import TopNews from '../TopNews';
-import SearchArticles from '../SearchArticles';
-import Categories from '../Categories';
-import ArticleDetails from '../ArticleDetails';
-import NotFound from '../NotFound';
 import { NewsRouteProps } from '../../types/types';
+const TopNews = lazy(() => import('../TopNews'));
+const SearchArticles = lazy(() => import('../SearchArticles'));
+const Categories = lazy(() => import('../Categories'));
+const ArticleDetails = lazy(() => import('../ArticleDetails'));
+const NotFound = lazy(() => import('../NotFound'));
 
 const NewsRoutes = ({
   country,
@@ -20,59 +21,61 @@ const NewsRoutes = ({
 }: NewsRouteProps) => {
   return (
     <>
-      <Routes>
-        <Route
-          path="*"
-          element={<NotFound setSelectedRoute={setSelectedRoute} />}
-        />
-        <Route
-          path="/"
-          element={
-            <TopNews
-              country={country}
-              newsData={data}
-              selectArticle={selectArticle}
-            />
-          }
-        />
-        <Route
-          path="/categories"
-          element={
-            <Categories
-              country={country}
-              selectCategories={selectCategories}
-              category={category}
-              newsData={data}
-              selectArticle={selectArticle}
-              setSelectedRoute={setSelectedRoute}
-            />
-          }
-        />
-        <Route
-          path="/search"
-          element={
-            <SearchArticles
-              country={country}
-              searchArticles={searchArticles}
-              searchTerm={searchTerm}
-              newsData={data}
-              selectArticle={selectArticle}
-              setSelectedRoute={setSelectedRoute}
-            />
-          }
-        />
-        <Route
-          path="/details/:id"
-          element={
-            <ArticleDetails
-              selectedArticle={selectedArticle}
-              selectCategories={selectCategories}
-              searchArticles={searchArticles}
-              setSelectedRoute={setSelectedRoute}
-            />
-          }
-        />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route
+            path="*"
+            element={<NotFound setSelectedRoute={setSelectedRoute} />}
+          />
+          <Route
+            path="/"
+            element={
+              <TopNews
+                country={country}
+                newsData={data}
+                selectArticle={selectArticle}
+              />
+            }
+          />
+          <Route
+            path="/categories"
+            element={
+              <Categories
+                country={country}
+                selectCategories={selectCategories}
+                category={category}
+                newsData={data}
+                selectArticle={selectArticle}
+                setSelectedRoute={setSelectedRoute}
+              />
+            }
+          />
+          <Route
+            path="/search"
+            element={
+              <SearchArticles
+                country={country}
+                searchArticles={searchArticles}
+                searchTerm={searchTerm}
+                newsData={data}
+                selectArticle={selectArticle}
+                setSelectedRoute={setSelectedRoute}
+              />
+            }
+          />
+          <Route
+            path="/details/:id"
+            element={
+              <ArticleDetails
+                selectedArticle={selectedArticle}
+                selectCategories={selectCategories}
+                searchArticles={searchArticles}
+                setSelectedRoute={setSelectedRoute}
+              />
+            }
+          />
+        </Routes>
+      </Suspense>
     </>
   );
 };
